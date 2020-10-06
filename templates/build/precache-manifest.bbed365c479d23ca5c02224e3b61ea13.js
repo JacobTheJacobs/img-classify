8aa25429510fc7b84ff4d9b0b6f34d656ab1c7c6
unknown <jake.yanov@gmail.com>
2020-10-06 21:22:06 +0300

    Commit message

---
+++ templates/build/precache-manifest.bbed365c479d23ca5c02224e3b61ea13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5725ba5a6343dff4e1e8dcbb4aee61ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eafdb1aa45ab5374a800",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "da8164ee99c8d104d40e",
+    "url": "/static/css/main.ee54ffed.chunk.css"
+  },
+  {
+    "revision": "eafdb1aa45ab5374a800",
+    "url": "/static/js/2.12db8e70.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.12db8e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8164ee99c8d104d40e",
+    "url": "/static/js/main.2a6379da.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
